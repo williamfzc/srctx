1ef31eaf840ba0f589a738294b75fe7fa171fa4e
williamfzc <178894043@qq.com>
2023-04-22 17:11:47 +0800

    feat: function level enhancement

--- Makefile
+++ Makefile
@@ -10,17 +10,17 @@ default:
 
 # linux
 build_linux_amd64:
-	GOOS=linux GOARCH=amd64 ${GOCMD} build -o srctx_linux_amd64 ./cmd/srctx
+	CGO_ENABLED=1 GOOS=linux GOARCH=amd64 ${GOCMD} build -o srctx_linux_amd64 ./cmd/srctx
 
 # windows
 build_windows_amd64:
-	GOOS=windows GOARCH=amd64 ${GOCMD} build -o srctx_windows_amd64.exe ./cmd/srctx
+	CGO_ENABLED=1 GOOS=windows GOARCH=amd64 ${GOCMD} build -o srctx_windows_amd64.exe ./cmd/srctx
 
 # mac
 build_macos_amd64:
-	GOOS=darwin GOARCH=amd64 ${GOCMD} build -o srctx_macos_amd64 ./cmd/srctx
+	CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 ${GOCMD} build -o srctx_macos_amd64 ./cmd/srctx
 build_macos_arm64:
-	GOOS=darwin GOARCH=arm64 ${GOCMD} build -o srctx_macos_arm64 ./cmd/srctx
+	CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 ${GOCMD} build -o srctx_macos_arm64 ./cmd/srctx
 
 test:
 	$(GOTEST) ./...
--- cmd/srctx/object.go
+++ cmd/srctx/object.go
@@ -11,13 +11,28 @@ type ReferenceScope struct {
 	CrossDirRefCount  int `json:"crossDirRefCount"`
 }
 
+type FuncReferenceScope struct {
+	TotalFuncRefCount     int `json:"totalFuncRefCount"`
+	CrossFuncFileRefCount int `json:"crossFuncFileRefCount"`
+	CrossFuncDirRefCount  int `json:"crossFuncDirRefCount"`
+}
+
 func (r *ReferenceScope) IsSafe() bool {
 	return r.TotalRefCount == 0 && r.CrossFileRefCount == 0 && r.CrossDirRefCount == 0
 }
 
+func (fr *FuncReferenceScope) IsSafe() bool {
+	return fr.TotalFuncRefCount == 0 && fr.CrossFuncFileRefCount == 0 && fr.CrossFuncDirRefCount == 0
+}
+
 type LineStat struct {
 	*FileScope
-	RefScope *ReferenceScope `json:"ref"`
+	RefScope     *ReferenceScope     `json:"ref"`
+	FuncRefScope *FuncReferenceScope `json:"funcRef"`
+}
+
+func (ls *LineStat) IsSafe() bool {
+	return ls.RefScope.IsSafe() && ls.FuncRefScope.IsSafe()
 }
 
 func NewLineStat(fileName string, lineNumber int) *LineStat {
@@ -29,6 +44,11 @@ func NewLineStat(fileName string, lineNumber int) *LineStat {
 		RefScope: &ReferenceScope{
 			CrossFileRefCount: 0,
 			CrossDirRefCount:  0,
+		},
+		FuncRefScope: &FuncReferenceScope{
+			TotalFuncRefCount:     0,
+			CrossFuncFileRefCount: 0,
+			CrossFuncDirRefCount:  0,
 		},
 	}
 }
