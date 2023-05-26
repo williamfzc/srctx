05424279309d2626f090846fa45de3c630aaa9c3
williamfzc <178894043@qq.com>
2023-05-26 23:57:18 +0800

    feat(#24): quickstart.sh

---
+++ scripts/quickstart.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+# Set version number and base url
+version="0.7.1"
+base_url="https://github.com/williamfzc/srctx/releases/download"
+
+echo "Starting SRCTX..."
+
+# Download and unpack scip-java.zip
+if [[ ! -f "scip-java.zip" ]]; then
+  echo "Downloading scip-java.zip..."
+  wget "${base_url}/v${version}/scip-java.zip"
+fi
+echo "Extracting scip-java.zip..."
+unzip -o scip-java.zip
+
+# Determine the filename of the srctx executable based on the OS type and architecture
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OS
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    # M1 Mac
+    filename="srctx-darwin-arm64"
+  else
+    # Intel Mac
+    filename="srctx-darwin-amd64"
+  fi
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  if [[ "$(uname -m)" == "aarch64" ]]; then
+    # ARM 64
+    filename="srctx-linux-arm64"
+  else
+    # x86_64
+    filename="srctx-linux-amd64"
+  fi
+elif [[ "$OSTYPE" == "msys" ]]; then
+  # Windows
+  filename="srctx-windows-amd64.exe"
+else
+  echo "Unsupported OS type: $OSTYPE"
+  exit 1
+fi
+
+# Check if the srctx executable already exists
+if [[ ! -f "srctx_bin" ]]; then
+  echo "Downloading srctx executable..."
+  # Download the srctx executable
+  wget "${base_url}/v${version}/${filename}" -O srctx_bin
+  chmod +x srctx_bin
+fi
+
+# Run srctx on the specified file or directory, with language parameter
+if [[ "$SRCTX_LANG" == "GOLANG" ]]; then
+  echo "Running srctx for Golang..."
+  ./srctx_bin diff --lang GOLANG --withIndex --src "$SRCTX_SRC"
+elif [[ "$SRCTX_LANG" == "JAVA" ]]; then
+  echo "Running srctx for Java..."
+  ./scip-java index "$SRCTX_BUILD_CMD"
+  ./srctx_bin diff --lang JAVA --src "$SRCTX_SRC"
+elif [[ "$SRCTX_LANG" == "KOTLIN" ]]; then
+  echo "Running srctx for Kotlin..."
+  ./scip-java index "$SRCTX_BUILD_CMD"
+  ./srctx_bin diff --lang KOTLIN --src "$SRCTX_SRC"
+else
+  echo "Unsupported language: $SRCTX_LANG"
+  exit 1
+fi
+
+echo "SRCTX finished successfully."
