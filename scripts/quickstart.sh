faa07756de8fd4ac5ee0b5cfdbca6352a183297d
williamfzc <178894043@qq.com>
2023-10-05 00:00:55 +0800

    chore: bump version to 0.11.0

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Set version number and base url
-version="0.10.5"
+version="0.11.0"
 base_url="https://github.com/williamfzc/srctx/releases/download"
 
 echo "Starting SRCTX..."
--- version.go
+++ version.go
@@ -17,6 +17,6 @@ limitations under the License.
 package srctx
 
 const (
-	Version = "v0.10.5"
+	Version = "v0.11.0"
 	RepoUrl = "https://github.com/williamfzc/srctx"
 )
